455ec3b1063afa4e227afe429e2f3744a2728105
Nigel Horne <njh@bandsman.co.uk>
2016-10-29 10:53:13 -0400

    Release 0.26

--- lib/HTML/SocialMedia.pm
+++ lib/HTML/SocialMedia.pm
@@ -10,11 +10,11 @@ HTML::SocialMedia - Put social media links into your website
 
 =head1 VERSION
 
-Version 0.25
+Version 0.26
 
 =cut
 
-our $VERSION = '0.25';
+our $VERSION = '0.26';
 
 =head1 SYNOPSIS
 
